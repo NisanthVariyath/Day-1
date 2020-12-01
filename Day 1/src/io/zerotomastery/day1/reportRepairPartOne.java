a5cd43d19ae5ff25ac4789f9877b379804baa301
Nisanth Variyath <73890016+NisanthVariyath@users.noreply.github.com>
2020-12-01 18:29:54 -0500

    Add files via upload

---
+++ Day 1/src/io/zerotomastery/day1/reportRepairPartOne.java
@@ -0,0 +1,265 @@
+package io.zerotomastery.day1;
+
+import java.util.ArrayList;
+
+public class reportRepairPartOne {
+	static int resultOf2Entries = 0;
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> reportEntries = new ArrayList<>();
+		ArrayList<Integer> reportFlag = new ArrayList<>();
+
+		reportEntries.add(1778);
+		reportEntries.add(1845);
+		reportEntries.add(1813);
+		reportEntries.add(1889);
+		reportEntries.add(1939);
+		reportEntries.add(1635);
+		reportEntries.add(1443);
+		reportEntries.add(796);
+		reportEntries.add(1799);
+		reportEntries.add(938);
+		reportEntries.add(1488);
+		reportEntries.add(1922);
+		reportEntries.add(1909);
+		reportEntries.add(1258);
+		reportEntries.add(1659);
+		reportEntries.add(1959);
+		reportEntries.add(1557);
+		reportEntries.add(1085);
+		reportEntries.add(1379);
+		reportEntries.add(1174);
+		reportEntries.add(1782);
+		reportEntries.add(1482);
+		reportEntries.add(1702);
+		reportEntries.add(1180);
+		reportEntries.add(1992);
+		reportEntries.add(1815);
+		reportEntries.add(1802);
+		reportEntries.add(215);
+		reportEntries.add(1649);
+		reportEntries.add(782);
+		reportEntries.add(1847);
+		reportEntries.add(1823);
+		reportEntries.add(1836);
+		reportEntries.add(1447);
+		reportEntries.add(1603);
+		reportEntries.add(1767);
+		reportEntries.add(1891);
+		reportEntries.add(1964);
+		reportEntries.add(1881);
+		reportEntries.add(1637);
+		reportEntries.add(1229);
+		reportEntries.add(1994);
+		reportEntries.add(1901);
+		reportEntries.add(1583);
+		reportEntries.add(1918);
+		reportEntries.add(1415);
+		reportEntries.add(1666);
+		reportEntries.add(1155);
+		reportEntries.add(1446);
+		reportEntries.add(1315);
+		reportEntries.add(1345);
+		reportEntries.add(1948);
+		reportEntries.add(1427);
+		reportEntries.add(1242);
+		reportEntries.add(1088);
+		reportEntries.add(807);
+		reportEntries.add(1747);
+		reportEntries.add(1514);
+		reportEntries.add(1351);
+		reportEntries.add(1791);
+		reportEntries.add(1612);
+		reportEntries.add(1550);
+		reportEntries.add(1926);
+		reportEntries.add(1455);
+		reportEntries.add(85);
+		reportEntries.add(1594);
+		reportEntries.add(1965);
+		reportEntries.add(1884);
+		reportEntries.add(1677);
+		reportEntries.add(1960);
+		reportEntries.add(1631);
+		reportEntries.add(1585);
+		reportEntries.add(1472);
+		reportEntries.add(1263);
+		reportEntries.add(1566);
+		reportEntries.add(1998);
+		reportEntries.add(1698);
+		reportEntries.add(1968);
+		reportEntries.add(1927);
+		reportEntries.add(1378);
+		reportEntries.add(1346);
+		reportEntries.add(1710);
+		reportEntries.add(1921);
+		reportEntries.add(1827);
+		reportEntries.add(1869);
+		reportEntries.add(1187);
+		reportEntries.add(1985);
+		reportEntries.add(1323);
+		reportEntries.add(1225);
+		reportEntries.add(1474);
+		reportEntries.add(1179);
+		reportEntries.add(1580);
+		reportEntries.add(1098);
+		reportEntries.add(1737);
+		reportEntries.add(1483);
+		reportEntries.add(1665);
+		reportEntries.add(1445);
+		reportEntries.add(1979);
+		reportEntries.add(1754);
+		reportEntries.add(1854);
+		reportEntries.add(1897);
+		reportEntries.add(1405);
+		reportEntries.add(1912);
+		reportEntries.add(1614);
+		reportEntries.add(1390);
+		reportEntries.add(1773);
+		reportEntries.add(1493);
+		reportEntries.add(1333);
+		reportEntries.add(1758);
+		reportEntries.add(1867);
+		reportEntries.add(1586);
+		reportEntries.add(1347);
+		reportEntries.add(1723);
+		reportEntries.add(1285);
+		reportEntries.add(394);
+		reportEntries.add(1743);
+		reportEntries.add(1252);
+		reportEntries.add(320);
+		reportEntries.add(1547);
+		reportEntries.add(1804);
+		reportEntries.add(1899);
+		reportEntries.add(1526);
+		reportEntries.add(1739);
+		reportEntries.add(1533);
+		reportEntries.add(1938);
+		reportEntries.add(1081);
+		reportEntries.add(1465);
+		reportEntries.add(1920);
+		reportEntries.add(1265);
+		reportEntries.add(1470);
+		reportEntries.add(1792);
+		reportEntries.add(1118);
+		reportEntries.add(1842);
+		reportEntries.add(1204);
+		reportEntries.add(1760);
+		reportEntries.add(1663);
+		reportEntries.add(893);
+		reportEntries.add(1853);
+		reportEntries.add(1244);
+		reportEntries.add(1256);
+		reportEntries.add(1428);
+		reportEntries.add(1334);
+		reportEntries.add(1967);
+		reportEntries.add(1249);
+		reportEntries.add(1752);
+		reportEntries.add(1124);
+		reportEntries.add(1725);
+		reportEntries.add(1949);
+		reportEntries.add(1340);
+		reportEntries.add(1205);
+		reportEntries.add(1584);
+		reportEntries.add(548);
+		reportEntries.add(1947);
+		reportEntries.add(2002);
+		reportEntries.add(1993);
+		reportEntries.add(1931);
+		reportEntries.add(1236);
+		reportEntries.add(1154);
+		reportEntries.add(1572);
+		reportEntries.add(1650);
+		reportEntries.add(1678);
+		reportEntries.add(1944);
+		reportEntries.add(1868);
+		reportEntries.add(1129);
+		reportEntries.add(1911);
+		reportEntries.add(1106);
+		reportEntries.add(1900);
+		reportEntries.add(1240);
+		reportEntries.add(1955);
+		reportEntries.add(1219);
+		reportEntries.add(1893);
+		reportEntries.add(1459);
+		reportEntries.add(1556);
+		reportEntries.add(1173);
+		reportEntries.add(1924);
+		reportEntries.add(1568);
+		reportEntries.add(1950);
+		reportEntries.add(1303);
+		reportEntries.add(1886);
+		reportEntries.add(1365);
+		reportEntries.add(1402);
+		reportEntries.add(1711);
+		reportEntries.add(1706);
+		reportEntries.add(1671);
+		reportEntries.add(1866);
+		reportEntries.add(1403);
+		reportEntries.add(1816);
+		reportEntries.add(1717);
+		reportEntries.add(1674);
+		reportEntries.add(1487);
+		reportEntries.add(1840);
+		reportEntries.add(1951);
+		reportEntries.add(1255);
+		reportEntries.add(1786);
+		reportEntries.add(1111);
+		reportEntries.add(1280);
+		reportEntries.add(1625);
+		reportEntries.add(1478);
+		reportEntries.add(1453);
+
+		// -----------------------------
+
+		reportRepairPartOne listObject = new reportRepairPartOne();
+		listObject.displayList(reportEntries);
+		for (int number : reportEntries) {
+			reportFlag.add(0);
+		}
+
+		for (int index = 0; index < reportEntries.size(); index++) {
+
+			listObject.find2Entries(reportEntries, reportFlag, reportEntries.get(index), index);
+		}
+		System.out.println("-------------------------------");
+		if (resultOf2Entries > 0) {
+			System.out.println("Solution for part1 : " + resultOf2Entries);
+		}
+
+	}
+
+	void displayList(ArrayList<Integer> value) {
+		for (int num : value) {
+			System.out.println(num);
+
+		}
+	}
+
+	void find2Entries(ArrayList<Integer> value, ArrayList<Integer> flag, int number, int index) {
+		
+		
+		for (int num = 0; num < value.size(); num++) {
+			if (((number + value.get(num)) == 2020) && (flag.get(num) == 0) && (flag.get(index) == 0)) {
+				resultOf2Entries = resultOf2Entries + (number * value.get(num));
+				modifyFlag(num, 1, flag);
+				
+
+			}
+
+		}
+
+	}
+
+// modify flag by specify position
+	void modifyFlag(int position, int value, ArrayList<Integer> flag) {
+		flag.set(position, value);
+	}
+
+	public void find3Entries(ArrayList<Integer> reportEntries, ArrayList<Integer> reportFlag, Integer integer,
+			int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
